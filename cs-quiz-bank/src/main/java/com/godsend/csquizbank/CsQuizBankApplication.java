799fd452d714176348be752f4b17a684abc1d904
김병현(40) <129952654+sirloinbh@users.noreply.github.com>
2024-01-01 23:26:11 +0900

    Feat : 백 엔드, 프론트 엔드 개발환경 설정 백 엔드는 스프링 부트로 설정 프론트 엔드는 vite로 설정

---
+++ cs-quiz-bank/src/main/java/com/godsend/csquizbank/CsQuizBankApplication.java
@@ -0,0 +1,13 @@
+package com.godsend.csquizbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsQuizBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsQuizBankApplication.class, args);
+	}
+
+}
---
+++ cs-quiz-bank/src/test/java/com/godsend/csquizbank/CsQuizBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godsend.csquizbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsQuizBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
